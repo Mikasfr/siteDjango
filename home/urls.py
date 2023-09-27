99f6e5450fd6787a7c81098659c233c41703765d
michael <michaelreques@gmail.com>
2023-09-27 13:58:13 -0300

    teste

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from . import views
 urlpatterns = [
     path('ver_home/', views.ver_home, name="ver_home"),
     path('ver_home/adcionarItem/', views.adcionarItem, name="adcionarItem"),
-    path('ver_home/adcionarItem/itemAdcionado/', views.itemAdcionado, name="itemAdcionado")
+    path('ver_home/adcionarItem/itemAdcionado/', views.itemAdcionado, name="itemAdcionado"),
+    path('ver_home/remover_produto/<int:codigo>/', views.remover_produto, name='remover_produto'),
+    path('ver_home/editarProduto/<int:codigo>/', views.editarProduto, name='editarProduto')
 ]
--- home/views.py
+++ home/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Produtos
 from django import db
+from django.shortcuts import redirect
+from .forms import ProdutoForm
 
 # criando as funções e ligando com as urls onde serão executadas
 
 def ver_home(request):
-    return render(request, 'home.html')
+    produtos = Produtos.objects.all()
+    return render(request, 'home.html', {'produtos' : produtos})
 # linkando o html
 
 def adcionarItem(request):
@@ -24,3 +27,41 @@ def adcionarItem(request):
 
 def itemAdcionado(request):
     return render(request, 'itemAdcionado.html')
+
+def remover_produto(request, codigo):
+    try:
+        # Use o código do produto para encontrar o objeto do produto no banco de dados
+        produto = Produtos.objects.get(codigo=codigo)
+        
+        # Realize a remoção do produto
+        produto.delete()
+
+        # Redirecione de volta para a página de estoque após a remoção
+        return redirect('ver_home')
+
+    except Produtos.DoesNotExist:
+        # Lidar com o caso em que o produto não existe
+        # Você pode retornar uma página de erro ou uma mensagem apropriada aqui
+        pass
+
+def editarProduto(request, codigo):
+    try:
+        # Use o código do produto para encontrar o objeto do produto no banco de dados
+        produto = Produtos.objects.get(codigo=codigo)
+        if request.method == 'POST':
+            # Se o formulário for enviado, atualize os dados do produto com base nos dados do formulário
+            form = ProdutoForm(request.POST, instance=produto)
+            if form.is_valid():
+                form.save()
+                # Redirecione de volta para a página de estoque após a edição
+                return redirect('ver_home')
+        else:
+            # Se o formulário não foi enviado, crie um formulário pré-preenchido com os dados do produto
+            form = ProdutoForm(instance=produto)
+
+        return render(request, 'editarProduto.html', {'form': form, 'produtos': Produtos})
+
+    except Produtos.DoesNotExist:
+        # Lidar com o caso em que o produto não existe
+        # Você pode retornar uma página de erro ou uma mensagem apropriada aqui
+        pass
